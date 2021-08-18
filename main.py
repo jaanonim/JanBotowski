f1c3b3a2791e985860dac0cba44b9decdd014ced
jaanonim <mat8mro@gmail.com>
2021-08-18 18:16:04 +0200

    adding nice interface

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 import asyncio
 import time
 
+from PyInquirer import prompt
+
 from bot import Bot
 
 
 async def main():
     b = Bot()
+    while True:
+        v = prompt(
+            {
+                "type": "list",
+                "name": "a",
+                "message": "What you want to do?",
+                "choices": [
+                    {"name": "Debug", "value": 1},
+                    {"name": "Send Message", "value": 2},
+                    {"name": "Summarize", "value": 3},
+                    {"name": "Exit", "value": 4},
+                ],
+            }
+        )["a"]
 
-    await b.debug()
-    await b.send_message()
-    time.sleep(10)
-    await b.summarize()
+        if v == 1:
+            await b.debug()
+        elif v == 2:
+            await b.send_message()
+        elif v == 3:
+            await b.summarize()
+        else:
+            exit()
 
 
 asyncio.run(main())
